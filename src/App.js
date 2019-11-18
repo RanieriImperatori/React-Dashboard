6d26abbe0b7c505f971db3ae0e3a297b295b0162
Ranieri Miranda Imperatori <ranieri.imperatori@gmail.com>
2019-11-17 23:40:35 -0300

    Insert Consultas and Faturamento

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import './App.css';
 import MenuSuperior from './components/MenuSuperior/MenuSuperior';
+import Resumo from './components/Resumo/Resumo';
 
 
 class App extends Component {
@@ -8,6 +9,7 @@ class App extends Component {
     return (
       <div>
         <MenuSuperior/>
+        <Resumo/>
       </div>
   
     );
